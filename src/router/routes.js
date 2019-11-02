523ebcdc570eadcb841d56f4fdfa0d7582c77d2f
James Lovallo <patrickonparker@gmail.com>
2019-11-02 17:42:21 -0400

    I hate routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -7,41 +7,14 @@ const routes = [
     children: [
       { path: '*', component: () => import('pages/Page.vue') }
     ]
+  },
+  {
+    path: '*/*',
+    component: () => import('layouts/MyLayout.vue'),
+    children: [
+      { path: '*', component: () => import('pages/Page.vue') }
+    ]
   }
 ]
-
-axios
-  .get(
-    `https://api.storyblok.com/v1/cdn/spaces/me?token=${process.env.SB_TOKEN}`
-  )
-  .then(space_res => {
-    // timestamp of latest publish
-    cache_version = space_res.data.space.version
-
-    // Call for all Links using the Links API: https://www.storyblok.com/docs/Delivery-Api/Links
-    axios
-      .get(
-        `https://api.storyblok.com/v1/cdn/links?token=${process.env.SB_TOKEN}&version=${version}&cv=${cache_version}`
-      )
-      .then(res => {
-        Object.keys(res.data.links).forEach(key => {
-          if (res.data.links[key].slug != 'home') {
-            let route = {
-              path: '/' + res.data.links[key].slug,
-              component: () => import('layouts/MyLayout.vue'),
-              children: [
-                {
-                  path: '',
-                  component: () => import('pages/Page.vue')
-                }
-              ]
-            }
-            routes.push(route)
-          }
-        })
-
-        callback(null, routes)
-      })
-  })
 
 export default routes
