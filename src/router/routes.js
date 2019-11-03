8c0a5265bca7fc3728811cac3c8ff2b6a82c9098
James Lovallo <patrickonparker@gmail.com>
2019-11-02 23:34:00 -0400

    Increasing levels of fuckery

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,20 +1,39 @@
-import axios from 'axios'
-
-const routes = [
+let routes = [
   {
-    path: '*',
+    path: '/',
     component: () => import('layouts/MyLayout.vue'),
     children: [
-      { path: '*', component: () => import('pages/Page.vue') }
+      { path: '', component: () => import('pages/Page.vue') }
     ]
   },
   {
-    path: '*/*',
+    path: '/*',
     component: () => import('layouts/MyLayout.vue'),
     children: [
-      { path: '*', component: () => import('pages/Page.vue') }
+      { path: '', component: () => import('pages/Page.vue') }
     ]
   }
 ]
+
+async function getLinks() {
+  const query = await fetch(
+    'https://api.storyblok.com/v1/cdn/links?token=BQbCs8YukzGveNbBLlyoSgtt'
+  )
+  const data = await query.json()
+  const links = data.links
+  for (var key in links) {
+    if (links.hasOwnProperty(key)) {
+      routes.push({
+        path: links[key].real_path,
+        component: () => import('layouts/MyLayout.vue'),
+        children: [
+          { path: '', component: () => import('pages/Page.vue') }
+        ]
+      })
+    }
+  }
+}
+
+getLinks()
 
 export default routes
