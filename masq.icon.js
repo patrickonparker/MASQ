a555074fdbbc30ad787369d435890e05c0c30a7d
James Lovallo <james.lovallo@mrm-mccann.com>
2019-11-18 16:28:29 -0500

    [skip ci] feature: updated build scripts to be hawt as hell

--- masq.icon.js
+++ masq.icon.js
@@ -2,10 +2,7 @@ var fs = require('fs'),
   request = require('request')
 
 var download = function(uri, filename, callback) {
-  request.head(uri, function(err, res, body) {
-    console.log('content-type:', res.headers['content-type'])
-    console.log('content-length:', res.headers['content-length'])
-
+  request.head(uri, () => {
     request(uri)
       .pipe(fs.createWriteStream(filename))
       .on('close', callback)
--- quasar.conf.js
+++ quasar.conf.js
@@ -72,16 +72,7 @@ module.exports = function(ctx) {
           request = require('request')
 
         var download = function(uri, filename, callback) {
-          request.head(uri, function(err, res, body) {
-            console.log(
-              'content-type:',
-              res.headers['content-type']
-            )
-            console.log(
-              'content-length:',
-              res.headers['content-length']
-            )
-
+          request.head(uri, () => {
             request(uri)
               .pipe(fs.createWriteStream(filename))
               .on('close', callback)
