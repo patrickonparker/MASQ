5621079bdd4d10a0d565fd78e57b312af5bfc364
James Lovallo <james.lovallo@mrm-mccann.com>
2019-11-18 16:34:36 -0500

    [skip ci] tweak: removed icon updater from quasar.conf

--- quasar.conf.js
+++ quasar.conf.js
@@ -67,25 +67,7 @@ module.exports = function(ctx) {
       // extractCSS: false,
 
       // https://quasar.dev/quasar-cli/cli-documentation/handling-webpack
-      extendWebpack(cfg) {
-        var fs = require('fs'),
-          request = require('request')
-
-        var download = function(uri, filename, callback) {
-          request.head(uri, () => {
-            request(uri)
-              .pipe(fs.createWriteStream(filename))
-              .on('close', callback)
-          })
-        }
-
-        const icon = process.env.APP_ICON
-        // console.log(icon)
-        download(icon, 'app-icon.png', function() {
-          console.log('Fetched icon:', icon)
-        })
-      }
-    },
+      extendWebpack(cfg) {},
 
     // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-devServer
     devServer: {
