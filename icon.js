2be6bc8957e00e5c6dfbb31748c9fb8353d717f4
James Lovallo <james.lovallo@mrm-mccann.com>
2019-11-21 14:55:12 -0500

    [skip ci] feature: restored script updates

---
+++ icon.js
@@ -0,0 +1,16 @@
+var fs = require("fs"),
+  request = require("request");
+
+var download = function(uri, filename, callback) {
+  request.head(uri, () => {
+    request(uri)
+      .pipe(fs.createWriteStream(filename))
+      .on("close", callback);
+  });
+};
+
+const icon = process.env.APP_ICON;
+// console.log(icon)
+download(icon, "app-icon.png", function() {
+  console.log("Fetched icon:", icon);
+});
