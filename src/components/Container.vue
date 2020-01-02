799c49b8358716af455a2cd2c168546f2e955d4c
James Lovallo <james.lovallo@mrm-mccann.com>
2020-01-02 16:00:44 -0500

    [skip ci] feature: added full-width option to container

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -2,7 +2,8 @@
 	<div
 		:class="[
 			'container',
-			`${blok.disable_bottom_margin ? '' : 'standard-margin'}`,
+			blok.disable_bottom_margin ? '' : 'standard-margin',
+			blok.full_width ? 'full-width-container' : '',
 			blok.class
 		]"
 		:style="blok.style"
@@ -19,7 +20,10 @@
 
 <script>
 	export default {
-		props: ["blok"]
+		props: ["blok"],
+		created() {
+			console.log(this.blok.full_width);
+		}
 	};
 </script>
 
@@ -31,5 +35,20 @@
 	}
 	.standard-margin {
 		padding-bottom: 45px;
+	}
+	.full-width-container {
+		max-width: none;
+		@media (max-width: 599px) {
+			margin: 0 -8px;
+			width: calc(100% + 16px);
+		}
+		@media (min-width: 600px) and (max-width: 1439px) {
+			margin: 0 -16px;
+			width: calc(100% + 32px);
+		}
+		@media (min-width: 1440px) {
+			margin: 0 -24px;
+			width: calc(100% + 48px);
+		}
 	}
 </style>
