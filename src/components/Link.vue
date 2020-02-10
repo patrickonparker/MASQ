2699ebe1df42fa8cc55c21e7f8410dc77be47732
James Lovallo <james.lovallo@mrm-mccann.com>
2020-02-10 14:35:11 -0500

    [skip ci] feature: added non-router link handling to Link.vue

--- src/components/Link.vue
+++ src/components/Link.vue
@@ -1,15 +1,27 @@
 <template>
-	<router-link :to="blok.to"
-		><component
-			v-for="blok in blok.content"
-			:key="blok._uid"
-			:is="blok.component"
-			:blok="blok"
-	/></router-link>
+	<Fragment>
+		<a v-if="blok.to.startsWith('http')" :href="blok.to"
+			><component
+				v-for="blok in blok.content"
+				:key="blok._uid"
+				:is="blok.component"
+				:blok="blok"
+		/></a>
+		<router-link v-else :to="blok.to"
+			><component
+				v-for="blok in blok.content"
+				:key="blok._uid"
+				:is="blok.component"
+				:blok="blok"
+		/></router-link>
+	</Fragment>
 </template>
 
 <script>
+	import { Fragment } from "vue-fragment";
+
 	export default {
-		props: ["blok"]
+		props: ["blok"],
+		components: { Fragment }
 	};
 </script>
